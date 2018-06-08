3a9f7c4373ad349fffd306141537ced69dbc36f2
Mike Castagna <mcastagna@knights.ucf.edu>
2018-06-07 22:23:40 -0400

    Add files via upload

---
+++ PredictParser/PredictParser/MarketContract.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictParser
+{
+    public class MarketContract
+    {
+        public int ID { get; set; }
+        public string DateEnd { get; set; }
+        public string Image { get; set; }
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public string LongName { get; set; }
+        public string ShortName { get; set; }
+        public string TickerSymbol { get; set; }
+        public string Status { get; set; }
+        public string LastTradePrice { get; set; }
+        public string BestBuyYesCost { get; set; }
+        public string BestBuyNoCost { get; set; }
+        public string BestSellYesCost { get; set; }
+        public string BestSellNoCost { get; set; }
+        public string LastClosePrice { get; set; }
+    }
+
+    public class MarketData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string TickerSymbol { get; set; }
+        public string Image { get; set; }
+        public string URL { get; set; }
+        public MarketContract[] Contracts { get; set; }
+        public string TimeStamp { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class MarketList
+    {
+        public MarketData[] Markets { get; set; }
+    }
+}
---
+++ PredictParser/PredictParser/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace PredictParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            while (true)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.predictit.org/api/marketdata/all/");
+                request.Accept = "application/xml";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Console.WriteLine(response.StatusDescription);
+
+                Stream dataStream = response.GetResponseStream();
+                XmlReader reader = XmlReader.Create(dataStream);
+
+                XmlSerializer serializer = new XmlSerializer(typeof(MarketList));
+                MarketList markets = (MarketList)serializer.Deserialize(reader);
+
+                foreach (MarketData market in markets.Markets)
+                {
+                    FindArb(market);
+                }
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                Thread.Sleep(60000);
+            }
+            
+        }
+
+        static void FindArb(MarketData market)
+        {
+            if (market.Contracts.Length == 1)
+                return;
+
+            double sum = 0;
+
+            foreach (MarketContract contract in market.Contracts)
+            {
+                if (contract.BestBuyYesCost != "")
+                    sum += Convert.ToDouble(contract.BestBuyYesCost);
+                else
+                    return;
+            }
+
+            if (sum < 0.9)
+            {
+                Console.WriteLine("Arb oppty exists in " + market.Name);
+            }
+        }
+    }
+}
